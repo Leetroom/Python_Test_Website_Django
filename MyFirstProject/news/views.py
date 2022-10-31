659f19d3aa78336f4d78a690a41e7c6df0cefe56
Leetroom <113527991+Leetroom@users.noreply.github.com>
2022-10-31 19:33:37 +0300

    Add categories
    Add categorty links

--- MyFirstProject/news/views.py
+++ MyFirstProject/news/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import News
+from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    return render(request, template_name='news/index.html', context={'news': news, 'title': 'Список новостей'})
+    categories = Category.objects.all()
+    return render(request, template_name='news/index.html', context={'news': news, 'title': 'Список новостей', 'categories': categories})
 
